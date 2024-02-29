e14b392d625e31e5e694d753f22a83ad461dd658
leeathena <hoyan.la@gmail.com>
2024-02-29 20:37:15 +0000

    basic set up

---
+++ Where-to-next/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <h1 className="title"> Where To Next? </h1>
+    </>
+  )
+}
+
+export default App
