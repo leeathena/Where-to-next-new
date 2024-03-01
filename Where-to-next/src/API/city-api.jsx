9f65bf3505a939d258eff131d6188016ae4cfd12
leeathena <hoyan.la@gmail.com>
2024-03-01 01:20:03 +0000

    city API partly done

---
+++ Where-to-next/src/API/city-api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'defed06cd4mshdbd60213603ec29p1af26ejsn03bbbbc275e5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
--- Where-to-next/src/components/search/search.jsx
+++ Where-to-next/src/components/search/search.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from '../../API/city-api';
 
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] =useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
 
     const handleOnChange = (seasrchData0) => {
         setSearch(searchData);
@@ -16,6 +24,7 @@ const Search = ({onSearchChange}) => {
             debounceTimeour={600}
             value={search}
             onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
