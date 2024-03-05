9b6a61f7f7bacb2074acbc4cbb7df2da951cc0ff
leeathena <hoyan.la@gmail.com>
2024-03-05 19:35:38 +0000

    added city name

--- Where-to-next/src/App.jsx
+++ Where-to-next/src/App.jsx
@@ -4,17 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header/Header';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
-import Forecast from './components/forecast/forecast';
 import { WEATHER_API_URL, WEATHER_API_KEY, CURRENCY_API_URL, CURRENCY_API_KEY } from './API/api';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
   const [currencyRate, setCurrencyRate] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
-    
+
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
     const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
     
@@ -40,7 +38,6 @@ function App() {
         const rate = currencyData.data[currencyCode]?.value; // Extracting the rate for the specific currency code
 
         setCurrentWeather({ city: searchData.label, ...weatherData });
-        setForecast({ city: searchData.label, ...forecastData });
         setCurrencyRate(rate); // Set the currency rate with the fetched data
       })
       .catch((err) => {
@@ -55,7 +52,6 @@ function App() {
         <Header />
         <Search onSearchChange={handleOnSearchChange} />
         {currentWeather && <CurrentWeather data={currentWeather} />}
-        {/* {forecast && <Forecast data={forecast} currencyRate={currencyRate} />} */}
         {currencyRate && <p>Currency Rate to GBP: {currencyRate}</p>}
       </div>
     </>
