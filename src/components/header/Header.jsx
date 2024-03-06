49cfb4a75b42007b160be7198e4accd43ff661d8
Temi Akindele <temiakindele@Temis-MacBook-Pro.local>
2024-03-06 15:06:52 +0000

    corrected the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,6 @@ function Header() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
             <div className="container-fluid">
-                {/* Replace the button toggler with the website title */}
                 <a className="navbar-brand col-lg-3 col-12 me-0 text-center" href="#">Where to Next?</a>
                 <div className="collapse navbar-collapse d-lg-flex justify-content-lg-end" id="navbarsExample11">
                     <div className="d-lg-flex col-lg-9 col-12 justify-content-lg-end">
