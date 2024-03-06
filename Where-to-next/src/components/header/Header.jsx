cfdfbe282f58032d9e5307bf3a009212177c5f4e
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-03-06 03:49:24 +0000

    fixed error xlink:href

--- Where-to-next/src/components/header/Header.jsx
+++ Where-to-next/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <svg className="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+                    <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap" /></svg>
                     <span className="fs-4">Where to Next?</span>
                 </a>
 
