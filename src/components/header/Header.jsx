78e52d26431cba80372d81db92ad05c53a352b04
Temi Akindele <temiakindele@Temis-MacBook-Pro.local>
2024-03-06 15:04:39 +0000

    replaced the button tag with the name of the website

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,13 @@
 import React from "react";
+import "./header.css";
 
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-                    <a className="navbar-brand col-lg-3 col-12 me-0" href="#">Where to Next?</a>
-
+                {/* Replace the button toggler with the website title */}
+                <a className="navbar-brand col-lg-3 col-12 me-0 text-center" href="#">Where to Next?</a>
+                <div className="collapse navbar-collapse d-lg-flex justify-content-lg-end" id="navbarsExample11">
                     <div className="d-lg-flex col-lg-9 col-12 justify-content-lg-end">
                         <button className="btn btn-primary">History</button>
                     </div>
