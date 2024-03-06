34d381b4dcc3b1f37817aeebacd8698989ba410b
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-03-06 04:40:26 +0000

    fixed problem with tag class

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -18,8 +18,8 @@ const CurrentWeather = ({ data }) => {
         <div className="p-5 text-center bg-body-tertiary rounded-3">
           <h1 className="city-name">{data.city}</h1>
           <h2 className="text-body-emphasis">Current Weather</h2>
-          <h3 class="mb-0">Temperature: {Math.round(data.main.temp)}°C</h3>
-          <div class="col-auto d-none d-lg-block">
+          <h3 className="mb-0">Temperature: {Math.round(data.main.temp)}°C</h3>
+          <div className="col-auto d-none d-lg-block">
           <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
             </div>
         </div>
